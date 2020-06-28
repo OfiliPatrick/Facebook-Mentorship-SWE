ff81bf65136f201176fee198d3462528d3c7a217
Patrick Ofili <patrickofili16@gmail.com>
2020-06-28 01:26:42 +0100

    cor

--- Hashtables/LRUcache.py
+++ Hashtables/LRUcache.py
@@ -1,37 +1,89 @@
-import collections
+class Node(object):
+    def __init__(self, node_key, val):
+        self.node_key = node_key
+        self.val = val
+        self.next = None
+        self.prev = None
 
-class LRUCache(object):
-    def __init__(self, size):
-        self.size = size
-        self.cache = collections.OrderedDict()
-
-    def get(self, key):
-        if key not in self.cache:
-            return - 1
-            
-        else:
-            self.cache.move_to_end(key, last=False)
-            return self.cache[key]
-
-
-    def add(self, key, val):
-        if len(self.cache) == self.size:
-            self.cache.popitem(last=True)
-
-        self.cache[key] = val
-        self.cache.move_to_end(key)
-
-            
-newLRUCache = 
+class LRUCache:
+    def __init__(self, max_size: int):
+        self.head = Node('head', 'head')
+        self.tail = Node('tail', 'tail')
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        self.max_size = max_size
+        self.curr_size = 0
+        self.node_lookup = {}
         
+    def printAll(self):
+        test = self.head
+        my_data = []
+        while test:
+            my_data.append(test.val)
+            test = test.next
+        return my_data, self.curr_size
+
+    def removeNode(self, node):
+        node_after = node.next
+        node_before = node.prev
+        node_before.next = node_after
+        node_after.prev = node_before      
+    
+    def moveToEnd(self, node):
+        prev_node_tail = self.tail.prev
+        prev_node_tail.next = node
+        node.prev = prev_node_tail
+        node.next = self.tail
+        self.tail.prev = node     
+
+    def get(self, node_key: int) -> int:     
+        if node_key not in self.node_lookup:
+            return -1
+        node = self.node_lookup[node_key]
+        node_val = node.val
+        self.removeNode(node)
+        self.moveToEnd(node)
+        return node_val
+        
+    def put(self, node_key: int, new_val: int) -> None:
+        if node_key in self.node_lookup:
+            node = self.node_lookup[node_key]
+            node.val = new_val
+            self.removeNode(node)
+            self.moveToEnd(node)   
+        else:
+            if self.curr_size >= self.max_size:
+                lru_node = self.head.next
+                del self.node_lookup[lru_node.node_key]
+                self.removeNode(lru_node)
+                self.curr_size -=1
+
+            new_node = Node(node_key, new_val)
+            self.node_lookup[node_key] = new_node
+            self.moveToEnd(new_node)
+            self.curr_size += 1
             
 
-cache = collections.OrderedDict()
 
-cache[1] = 2
-cache[3] = 4
-cache[5] = 6
-# cache.move_to_end(3, last=False)
+    
+# cache = LRUCache(2)
+# print(cache.printAll())
+# cache.put(2, 1)
+# cache.put(1, 1)
+# cache.put(2,3)      
+# cache.put(4,1)       
+# cache.get(1)       
+# cache.get(2)
 
-cache.popitem(last=True)
-print(cache)
+
+# cache.put(1, 1)
+# cache.put(2, 2)
+# # cache.put(3,3)
+# cache.get(1)      
+# cache.put(3, 3)    
+# cache.get(2)       
+# cache.put(4, 4)    
+# cache.get(1)       
+# cache.get(3)
+# cache.get(4)
+
