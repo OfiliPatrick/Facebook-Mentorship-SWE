ede2a0cfdbab1c0dbf2eb6b40fbfe728ec984680
Patrick Ofili <patrickofili16@gmail.com>
2020-07-04 01:36:33 +0100

    updated strings

--- Strings/AnagramCheck.py
+++ Strings/AnagramCheck.py
@@ -1,8 +1,34 @@
-def anagram(s1, s2):
-    sorted_s1 = sorted(s1)
-    sorted_s2 = sorted(s2)
+def isAnagram1(s1, s2):
+    if len(s1) != len(s2):
+        return False
+
+    hash_tab = {}
+    for char1 in s1:
+        if char1 in hash_tab:
+            hash_tab[char1] += 1
+            
+        else:
+            hash_tab[char1] = 1
+
+    for char2 in s2:
+        if char2 in hash_tab:
+            hash_tab[char2] -= 1
+            
+        else:
+            hash_tab[char2] = 1
+    for key in hash_tab:
+        if hash_tab[key] != 0:
+            return False
+    return True
+
+
+def isAnagram2(s1, s2):
+    if len(s1) != len(s2):
+        return False
+    sorted_s1 = "".join(sorted(s1))
+    sorted_s2 = "".join(sorted(s2)) 
     return sorted_s1 == sorted_s2
+    
 
-
-print(anagram('abcde', 'baedc'))
+print(isAnagram1('abcde', 'baedc'))
 
